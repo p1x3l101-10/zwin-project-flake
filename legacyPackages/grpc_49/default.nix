b222545093fcd1aca8279c0e1b9ee6b45179f213
Pixel <scott.blatt.0b10@gmail.com>
2025-03-01 22:45:07 -0600

    change src

--- legacyPackages/grpc_49/default.nix
+++ legacyPackages/grpc_49/default.nix
@@ -1,10 +1,13 @@
-{ grpc }:
+{ grpc
+, fetchFromGitHub
+}:
 
 grpc.overrideAttrs (oldAttrs: rec {
   version = "1.49.1";
-  src = oldAttrs.src.overrideAttrs (oldSrcAttrs: {
+  src = fetchFromGitHub {
+    inherit (oldAttrs.src) owner repo fetchSubmodules;
     rev = "v${version}";
     hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
-  });
+  };
   meta.description = oldAttrs.meta.description + " using old version for compat";
 })
