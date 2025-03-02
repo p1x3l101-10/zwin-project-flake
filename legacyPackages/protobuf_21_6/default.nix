e93da99947f1cbf73f63a5be58ed85cfe718ecac
Pixel <scott.blatt.0b10@gmail.com>
2025-03-01 22:28:07 -0600

    flesh out protobuf

---
+++ legacyPackages/protobuf_21_6/default.nix
@@ -0,0 +1,14 @@
+{ path
+, callPackage
+, ...
+}@args:
+
+# Use the nixpkgs template for protobuf
+
+callPackage ${path}/pkgs/development/libraries/protobuf/generic.nix (
+  {
+    version = "21.6";
+    hash = "sha256-VZQEFHq17UsTH5CZZOcJBKiScGV2xPJ/e6gkkVliRCU=";
+  }
+  // args
+)
