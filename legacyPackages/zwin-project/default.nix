26b2f6b8a3e41f127cf2271b101903a93992c847
Pixel <scott.blatt.0b10@gmail.com>
2025-03-02 11:47:11 -0600

    change ordering

--- legacyPackages/zwin-project/default.nix
+++ legacyPackages/zwin-project/default.nix
@@ -1,10 +1,11 @@
 { callPackage }:
 
-rec {
-  zen-object-viewer = callPackage ./zen-object-viewer { };
+{
   zen-protocols = callPackage ./zen-protocols { };
   zen-remote = callPackage ./zen-remote { };
-  zennist = callPackage ./zennist { };
   zukou = callPackage ./zukou { };
   zwin = callPackage ./zwin { };
+
+  zen-object-viewer = callPackage ./zen-object-viewer { };
+  zennist = callPackage ./zennist { };
 }
