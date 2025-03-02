646d20c16047039bfb7950b48e0a2355204904d3
Pixel <scott.blatt.0b10@gmail.com>
2025-03-02 11:50:32 -0600

    revert

--- legacyPackages/zwin-project/default.nix
+++ legacyPackages/zwin-project/default.nix
@@ -1,11 +1,10 @@
 { callPackage }:
 
 {
+  zen-object-viewer = callPackage ./zen-object-viewer { };
   zen-protocols = callPackage ./zen-protocols { };
   zen-remote = callPackage ./zen-remote { };
+  zennist = callPackage ./zennist { };
   zukou = callPackage ./zukou { };
   zwin = callPackage ./zwin { };
-
-  zen-object-viewer = callPackage ./zen-object-viewer { };
-  zennist = callPackage ./zennist { };
 }
