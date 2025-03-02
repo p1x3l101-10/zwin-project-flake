385bd566df4d9d6625e3c8749f4da69c1a3924a7
Pixel <scott.blatt.0b10@gmail.com>
2025-03-01 20:20:54 -0600

    close arg

--- legacyPackages/zwin-project/default.nix
+++ legacyPackages/zwin-project/default.nix
@@ -3,5 +3,5 @@
 lib.makeScope pkgs.newScope (self: with self; {
   zen-protocols = callPackage ./zen-protocols {};
   zwin = callPackage ./zwin {};
-  zukou = callPackage ./zukou { inherit (self) zwin };
+  zukou = callPackage ./zukou { inherit (self) zwin; };
 })
