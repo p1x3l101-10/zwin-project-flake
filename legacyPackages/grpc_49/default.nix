7948e4bcfa0e804eb523c31332b7d6040cd8023c
Pixel <scott.blatt.0b10@gmail.com>
2025-03-01 22:46:34 -0600

    fix hash

--- legacyPackages/grpc_49/default.nix
+++ legacyPackages/grpc_49/default.nix
@@ -7,7 +7,7 @@ grpc.overrideAttrs (oldAttrs: rec {
   src = fetchFromGitHub {
     inherit (oldAttrs.src) owner repo fetchSubmodules;
     rev = "v${version}";
-    hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+    hash = "sha256-xyhPyBtbY54qidjZSrPO8oRPA6EEVok/l42cY8DpER8=";
   };
   meta.description = oldAttrs.meta.description + " using old version for compat";
 })
