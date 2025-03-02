465b47df7ba5c7e61d3c434d9f2b5f11359dd2ff
Pixel <scott.blatt.0b10@gmail.com>
2025-03-01 19:43:59 -0600

    fix package

--- legacyPackages/zen-protocols/default.nix
+++ legacyPackages/zen-protocols/default.nix
@@ -6,18 +6,19 @@
 }:
 
 stdenv.mkDerivation rec {
-  pname = "zen-protocols";
+  name = "zen-protocols";
+  version = "0.0.0"; # No version upstream
 
   src = fetchFromGitHub {
     owner = "zwin-project";
-    repo = pname;
+    repo = name;
     rev = "918348103312f84a96a3961a3dde51713b93bc3d";
     hash = lib.fakeHash;
   };
 
   meta = with lib; {
     description = "Wayland protocols for Zen Desktop";
-    homepage = "https://github.com/zwin-project/" + pname;
+    homepage = "https://github.com/zwin-project/" + name;
     license = licenses.mit;
     platforms = platforms.linux;
   };
