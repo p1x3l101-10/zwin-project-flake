12f02a93ec9ed82bcbb2483107a9a7eb9bdf0ed5
Pixel <scott.blatt.0b10@gmail.com>
2025-03-01 21:17:52 -0600

    try new makeScope

--- legacyPackages/zwin-project/default.nix
+++ legacyPackages/zwin-project/default.nix
@@ -1,6 +1,6 @@
 { lib, pkgs }:
 
-lib.makeScope pkgs.newScope (prev: {
+lib.makeScope (lib.callPackageWith pkgs) (prev: {
   zen-protocols = prev.callPackage ./zen-protocols {};
   zwin = prev.callPackage ./zwin {};
   zukou = prev.callPackage ./zukou {};
