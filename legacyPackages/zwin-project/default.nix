f9443a3ba79878b21626cbfb89a03ee75d4737ef
Pixel <scott.blatt.0b10@gmail.com>
2025-03-01 21:24:32 -0600

    use clang for stdenv

--- legacyPackages/zwin-project/default.nix
+++ legacyPackages/zwin-project/default.nix
@@ -1,6 +1,8 @@
 { lib, pkgs }:
 
-lib.makeScope pkgs.newScope (prev: {
+lib.makeScope (pkgs.newScope {
+  stdenv = pkgs.clangStdenv;
+}) (prev: {
   zen-protocols = prev.callPackage ./zen-protocols {};
   zwin = prev.callPackage ./zwin {};
   zukou = prev.callPackage ./zukou {};
