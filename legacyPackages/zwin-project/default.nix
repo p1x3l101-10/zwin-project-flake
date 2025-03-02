22d3127cc51693b18f6ab5dcd96e2db4fd781e3e
Pixel <scott.blatt.0b10@gmail.com>
2025-03-01 21:19:38 -0600

    revert

--- legacyPackages/zwin-project/default.nix
+++ legacyPackages/zwin-project/default.nix
@@ -1,6 +1,6 @@
 { lib, pkgs }:
 
-lib.makeScope (lib.callPackageWith pkgs) (prev: {
+lib.makeScope pkgs.newScope (prev: {
   zen-protocols = prev.callPackage ./zen-protocols {};
   zwin = prev.callPackage ./zwin {};
   zukou = prev.callPackage ./zukou {};
