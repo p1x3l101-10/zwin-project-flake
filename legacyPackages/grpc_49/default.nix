3b9d1c3ce3938dbd41eaca36f4c5296a7fe2d041
Pixel <scott.blatt.0b10@gmail.com>
2025-03-01 22:55:49 -0600

    just use the old upstream one

--- legacyPackages/grpc_49/default.nix
+++ legacyPackages/grpc_49/default.nix
@@ -1,17 +1,39 @@
-{ grpc
+# Copied from nixpkgs rev: <d86e0e4f6c1375280a36252e61eeb3f8052e770b>
+{ lib
+, stdenv
 , fetchFromGitHub
 , fetchpatch
+, buildPackages
+, cmake
+, zlib
+, c-ares
+, pkg-config
+, re2
+, openssl
+, protobuf
+, grpc
+, abseil-cpp
+, libnsl
+
+# tests
+, python3
+, arrow-cpp
 }:
 
-# Copy data from nixpkgs rev: <d86e0e4f6c1375280a36252e61eeb3f8052e770b>
+stdenv.mkDerivation rec {
+  pname = "grpc";
+  version = "1.49.1"; # N.B: if you change this, please update:
+    # pythonPackages.grpcio-tools
+    # pythonPackages.grpcio-status
 
-grpc.overrideAttrs (oldAttrs: rec {
-  version = "1.49.1";
   src = fetchFromGitHub {
-    inherit (oldAttrs.src) owner repo fetchSubmodules;
+    owner = "grpc";
+    repo = "grpc";
     rev = "v${version}";
     hash = "sha256-xyhPyBtbY54qidjZSrPO8oRPA6EEVok/l42cY8DpER8=";
+    fetchSubmodules = true;
   };
+
   patches = [
     # Fix build on armv6l (https://github.com/grpc/grpc/pull/21341)
     (fetchpatch {
@@ -26,5 +48,61 @@ grpc.overrideAttrs (oldAttrs: rec {
       revert = true;
     })
   ];
-  meta.description = oldAttrs.meta.description + " using old version for compat";
-})
+
+  nativeBuildInputs = [ cmake pkg-config ]
+    ++ lib.optional (stdenv.hostPlatform != stdenv.buildPlatform) grpc;
+  propagatedBuildInputs = [ c-ares re2 zlib abseil-cpp ];
+  buildInputs = [ c-ares.cmake-config openssl protobuf ]
+    ++ lib.optionals stdenv.isLinux [ libnsl ];
+
+  cmakeFlags = [
+    "-DgRPC_ZLIB_PROVIDER=package"
+    "-DgRPC_CARES_PROVIDER=package"
+    "-DgRPC_RE2_PROVIDER=package"
+    "-DgRPC_SSL_PROVIDER=package"
+    "-DgRPC_PROTOBUF_PROVIDER=package"
+    "-DgRPC_ABSL_PROVIDER=package"
+    "-DBUILD_SHARED_LIBS=ON"
+  ] ++ lib.optionals (stdenv.hostPlatform != stdenv.buildPlatform) [
+    "-D_gRPC_PROTOBUF_PROTOC_EXECUTABLE=${buildPackages.protobuf}/bin/protoc"
+  ] ++ lib.optionals ((stdenv.hostPlatform.useLLVM or false) && lib.versionOlder stdenv.cc.cc.version "11.0") [
+    # Needs to be compiled with -std=c++11 for clang < 11. Interestingly this is
+    # only an issue with the useLLVM stdenv, not the darwin stdenv…
+    # https://github.com/grpc/grpc/issues/26473#issuecomment-860885484
+    "-DCMAKE_CXX_STANDARD=11"
+  ];
+
+  # CMake creates a build directory by default, this conflicts with the
+  # basel BUILD file on case-insensitive filesystems.
+  preConfigure = ''
+    rm -vf BUILD
+  '';
+
+  # When natively compiling, grpc_cpp_plugin is executed from the build directory,
+  # needing to load dynamic libraries from the build directory, so we set
+  # LD_LIBRARY_PATH to enable this. When cross compiling we need to avoid this,
+  # since it can cause the grpc_cpp_plugin executable from buildPackages to
+  # crash if build and host architecture are compatible (e. g. pkgsLLVM).
+  preBuild = lib.optionalString (stdenv.hostPlatform == stdenv.buildPlatform) ''
+    export LD_LIBRARY_PATH=$(pwd)''${LD_LIBRARY_PATH:+:}$LD_LIBRARY_PATH
+  '';
+
+  NIX_CFLAGS_COMPILE = lib.optionalString stdenv.cc.isClang "-Wno-error=unknown-warning-option"
+    + lib.optionalString stdenv.isAarch64 "-Wno-error=format-security";
+
+  enableParallelBuilds = true;
+
+  passthru.tests = {
+    inherit (python3.pkgs) grpcio-status grpcio-tools;
+    inherit arrow-cpp;
+  };
+
+  meta = with lib; {
+    description = "The C based gRPC (C++, Python, Ruby, Objective-C, PHP, C#)";
+    license = licenses.asl20;
+    maintainers = with maintainers; [ lnl7 marsam ];
+    homepage = "https://grpc.io/";
+    platforms = platforms.all;
+    changelog = "https://github.com/grpc/grpc/releases/tag/v${version}";
+  };
+}
