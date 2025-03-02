5c046acf66c92536ebdee6070c24f998e1047807
Pixel <scott.blatt.0b10@gmail.com>
2025-03-01 23:04:07 -0600

    add inputs

--- outputs.nix
+++ outputs.nix
@@ -3,9 +3,10 @@ let
   inherit (inputs) nixpkgs;
   pkgs = nixpkgs.legacyPackages.x86_64-linux;
   lib = nixpkgs.lib;
+  callModule = m: import m { inherit inputs lib pkgs; };
 in
 {
-  legacyPackages.x86_64-linux = import ./legacyPackages { inherit lib pkgs; };
+  legacyPackages.x86_64-linux = callModule ./legacyPackages;
   formatter.x86_64-linux = pkgs.nixpkgs-fmt;
-  overlays = import ./overlays { inherit inputs lib pkgs; };
+  overlays = callModule ./overlays;
 }
