6a6ca41f8779a36e0d23331bb39bdc30a579356a
Pixel <scott.blatt.0b10@gmail.com>
2025-03-02 18:26:52 -0600

    revert

--- legacyPackages/zwin-project/default.nix
+++ legacyPackages/zwin-project/default.nix
@@ -1,11 +1,11 @@
 { makeScope, newScope }:
 
 makeScope newScope (prev: {
+  zen = prev.callPackage ./zen { };
   zen-object-viewer = prev.callPackage ./zen-object-viewer { };
   zen-protocols = prev.callPackage ./zen-protocols { };
   zen-remote = prev.callPackage ./zen-remote { };
   zennist = prev.callPackage ./zennist { };
   zukou = prev.callPackage ./zukou { };
   zwin = prev.callPackage ./zwin { };
-  zen = prev.callPackage ./zen { };
 })
